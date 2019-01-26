a9ee98acd93899c9e6c4f1ad6ec13f321730ea0d
omotoy <omotoy@kagicomb.com>
2019-01-26 17:45:21 +0900

    シーン大本の制作

---
+++ Mother/Assets/Scripts/Omoto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Omoto : MonoBehaviour
+{
+    public Vector3 Force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Rigidbody>().AddForce(Force);
+    }
+}
