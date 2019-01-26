f5e2218c98c5e0d9a63bcb6efa8ee86515e38f3e
Kengo Ishihara <ishiharak@users.noreply.github.com>
2019-01-26 20:01:14 +0900

    ターゲットサイトの種類増やした＆切り替えシステム実装

---
+++ Mother/Assets/Prefabs/ChangeSiteImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSiteImage : MonoBehaviour
+{
+    public Sprite[] Images;
+
+    public void Default()
+    {
+        this.GetComponent<Image>().sprite = Images[0];
+    }
+
+    public void Hand()
+    {
+        this.GetComponent<Image>().sprite = Images[1];
+    }
+
+    public void Grub()
+    {
+        this.GetComponent<Image>().sprite = Images[2];
+    }
+
+    public void Climb()
+    {
+        this.GetComponent<Image>().sprite = Images[3];
+    }
+}
