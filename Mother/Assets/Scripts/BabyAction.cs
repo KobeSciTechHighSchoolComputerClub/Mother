d40ad21e6d1d8e4cee6b70617e50c275d133a6bc
omotoy <omotoy@kagicomb.com>
2019-01-26 18:06:09 +0900

    ものを動かせるようになったはず

---
+++ Mother/Assets/Scripts/BabyAction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabyAction : MonoBehaviour
+{
+    public LayerMask Mask;
+    public float Range;
+    public float CatchPower;
+    private GameObject CatchPoint;
+    private float catchLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CatchPoint = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(CatchPoint != null)
+            {
+                Destroy(CatchPoint);
+                return;
+            }
+
+            RaycastHit hit = new RaycastHit();
+            if (!Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Range, Mask)) return;
+            if (hit.collider.tag != "CanMove") return;
+            GameObject d = new GameObject();
+            d.transform.position = hit.point;
+            d.transform.parent = hit.collider.transform;
+            catchLength = hit.distance;
+        }
+
+        if(CatchPoint != null)
+        {
+            Vector3 point = this.transform.position + this.transform.forward * catchLength;
+            point = (point - CatchPoint.transform.position).normalized * CatchPower;
+        }
+    }
+}
