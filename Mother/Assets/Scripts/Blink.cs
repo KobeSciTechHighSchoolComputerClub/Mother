0b89dd5d529bf633f71a1267ec74e91667a3ce1c
Kengo Ishihara <ishiharak@users.noreply.github.com>
2019-01-27 10:54:33 +0900

    文字チカ

---
+++ Mother/Assets/Scripts/Blink.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour
+{
+    TMPro.TextMeshProUGUI textMeshProUGUI;
+    public float BlinkSpeed = 1f;
+
+    void Start()
+    {
+        textMeshProUGUI = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        textMeshProUGUI.color = new Color(textMeshProUGUI.color.r, textMeshProUGUI.color.g, textMeshProUGUI.color.b, Mathf.Abs(Mathf.Sin(Time.time * BlinkSpeed)));
+    }
+}
