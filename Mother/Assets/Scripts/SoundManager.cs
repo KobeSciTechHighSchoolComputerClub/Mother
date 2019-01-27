868ab7cf09a1f444391f8aa023597d4eac59aece
Kengo Ishihara <ishiharak@users.noreply.github.com>
2019-01-27 10:31:41 +0900

    タイトルにBGM

---
+++ Mother/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public void FadeIn()
+    {
+        GetComponent<AudioSource>().Play();
+        StartCoroutine(fadeIn(10f));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(fadeOut(1f));
+    }
+
+    IEnumerator fadeIn(float fadeTime)
+    {
+        float t = Time.time;
+        while (true)
+        {
+            float tt = Time.time - t;
+            if(tt > fadeTime)
+            {
+                break;
+            }
+            GetComponent<AudioSource>().volume = Mathf.Lerp(0f, 1f, tt / fadeTime);
+            yield return null;
+        }
+    }
+
+    IEnumerator fadeOut(float fadeTime)
+    {
+        float t = Time.time;
+        while (true)
+        {
+            float tt = Time.time - t;
+            if (tt > fadeTime)
+            {
+                GetComponent<AudioSource>().Stop();
+                break;
+            }
+            GetComponent<AudioSource>().volume = Mathf.Lerp(1f, 0f, tt / fadeTime);
+            yield return null;
+        }
+    }
+}
